249d6270b42bc9e379c9619f136038f3d72ed6a7
Olu <olaoyeleye@yahoo.co.uk>
2023-09-02 02:07:02 +0100

    i have updated the script to removesensitive password and deactivated the accesskeythat could lead to unwanted users

--- base_infrastructure/9-ec2.tf
+++ base_infrastructure/9-ec2.tf
@@ -90,7 +90,6 @@ sudo apt update -y
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
-
 sudo apt update -y
 sudo apt install -y
 
@@ -106,6 +105,30 @@ sudo apt update -y
 sudo usermod -aG docker $USER
 sudo apt install git -y
 
+sudo apt install  software-properties-common gnupg2 curl
+curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
+sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install terraform -y   
+
+
+
+
+sudo apt install openjdk-11-jdk -y
+
+#curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+#echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+
+# sudo gpg --keyserver  http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key --recv-keys 5BA31D57EF5975CA
+sudo apt update
+sudo apt install jenkins -y
+
+sudo systemctl enable --now jenkins
+sudo ufw allow 8080
+sudo ufw enable
 
 EOF
 }
