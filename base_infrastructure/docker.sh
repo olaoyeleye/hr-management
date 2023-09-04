ed7e49c9175129e55e5114cb6722628b70ce0d40
Olu <olaoyeleye@yahoo.co.uk>
2023-09-04 12:08:54 +0100

    i have added docker.sh file and updated secretfile to be used by jenkins in executing ansible-playbook

---
+++ base_infrastructure/docker.sh
@@ -0,0 +1,6 @@
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo apt update -y
+sudo usermod -aG docker $USER
