eca246b8258fa8add331f0092372e0c49378af7a
Olu <olaoyeleye@yahoo.co.uk>
2023-09-06 17:06:13 +0100

    added target group depends on the taget group id

--- base_infrastructure/11-target_group.tf
+++ base_infrastructure/11-target_group.tf
@@ -11,16 +11,25 @@ resource "aws_lb_target_group_attachment" "tg-attachment-1" {
   target_group_arn = aws_lb_target_group.tg-all.arn
   target_id        = aws_instance.hr-app-name-1.id
   port             = 80
+  depends_on = [
+    aws_lb_target_group.tg-all.id
+  ]
 }
 
 resource "aws_lb_target_group_attachment" "tg-attachment-2" {
   target_group_arn = aws_lb_target_group.tg-all.arn
   target_id        = aws_instance.hr-app-name-2.id
   port             = 80
+    depends_on = [
+    aws_lb_target_group.tg-all.id
+  ]
 }
 
 resource "aws_lb_target_group_attachment" "tg-attachment-3" {
   target_group_arn = aws_lb_target_group.tg-all.arn
   target_id        = aws_instance.hr-app-name-3.id
   port             = 80
+    depends_on = [
+    aws_lb_target_group.tg-all.id
+  ]
 }
