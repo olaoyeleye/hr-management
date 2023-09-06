d3a675cd97b8d0e4961d0783b72a680adc11561d
Olu <olaoyeleye@yahoo.co.uk>
2023-09-06 19:02:29 +0100

    added route 53 from 15

---
+++ base_infrastructure/15-route53.tf
@@ -0,0 +1,21 @@
+resource "aws_route53_zone" "hrapp-route53" {
+  name = "hrapp-kensko.com"
+    vpc {
+    vpc_id = aws_vpc.hr-app-vpc.id
+  }
+
+}
+
+
+resource "aws_route53_record" "hrapp-ns" {
+  zone_id = aws_route53_zone.hrapp-route53.zone_id
+  name    = "dev.hrapp-kensko.com"
+  type    = "NS"
+  ttl     = "30"
+  records = aws_route53_zone.hrapp-route53.name_servers
+}
+
+resource "aws_route53_zone_association" "secondary" {
+  zone_id = aws_route53_zone.hrapp-route53.zone_id
+  vpc_id  = aws_vpc.hr-app-vpc.id
+}
