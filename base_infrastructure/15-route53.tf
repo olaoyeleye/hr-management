9ee988b560f011bb08ce7034b4f97a23ee0fb5c8
Olu <olaoyeleye@yahoo.co.uk>
2023-09-19 21:42:11 +0100

    added inventory

--- base_infrastructure/15-route53.tf
+++ base_infrastructure/15-route53.tf
@@ -13,7 +13,7 @@ resource "aws_route53_zone" "hrapp-route53" {
 
 resource "aws_route53_record" "hrapp-ns-1" {
   zone_id = aws_route53_zone.hrapp-route53.zone_id
-  name    = "hrapp-1.kensko-hr-management.link"
+  name    = "hrapp-1.hr-management.link"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.hr-app-1.public_ip]
@@ -21,7 +21,7 @@ resource "aws_route53_record" "hrapp-ns-1" {
 
 resource "aws_route53_record" "hrapp-ns-2" {
   zone_id = aws_route53_zone.hrapp-route53.zone_id
-  name    = "hrapp-2.kensko-hr-management.link"
+  name    = "hrapp-2.hr-management.link"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.hr-app-2.public_ip]
@@ -29,7 +29,7 @@ resource "aws_route53_record" "hrapp-ns-2" {
 
 resource "aws_route53_record" "hrapp-ns-3" {
   zone_id = aws_route53_zone.hrapp-route53.zone_id
-  name    = "hrapp-3.kensko-hr-management.link"
+  name    = "hrapp-3.hr-management.link"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.hr-app-3.public_ip]
@@ -37,7 +37,7 @@ resource "aws_route53_record" "hrapp-ns-3" {
 
 resource "aws_route53_record" "hrapp-ns-4" {
   zone_id = aws_route53_zone.hrapp-route53.zone_id
-  name    = "prometheus.kensko-hr-management.link"
+  name    = "prometheus.hr-management.link"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.hr-app-4.public_ip]
@@ -47,7 +47,7 @@ resource "aws_route53_record" "hrapp-ns-4" {
 
 
 resource "aws_route53_health_check" "hrapp-health-check" {
-  fqdn              = "kensko-hr-management.link"
+  fqdn              = "hr-management.link"
   port              = 80
   type              = "HTTP"
   resource_path     = "/"
