4d337a1d41e67a198d5856375125976a22ca49c2
Olu <olaoyeleye@yahoo.co.uk>
2023-09-20 15:08:54 +0100

    added inventory

--- base_infrastructure/15-route53.tf
+++ base_infrastructure/15-route53.tf
@@ -1,10 +1,10 @@
-resource "aws_route53_zone" "hrapp-route53" {
-  name = "kensko-hr-management.link"
-    vpc {
-    vpc_id = aws_vpc.hr-app-vpc.id
-  }
+#resource "aws_route53_zone" "hrapp-route53" {
+#  name = "kensko-hr-management.link"
+#    vpc {
+ #   vpc_id = aws_vpc.hr-app-vpc.id
+ # }
 
-}
+#}
 
 
 
@@ -15,32 +15,32 @@ resource "aws_route53_zone" "hrapp-route53" {
 
 
 resource "aws_route53_record" "hrapp-ns-1" {
-  zone_id = aws_route53_zone.hrapp-route53.zone_id
-  name    = "hrapp-1.kensko-hr-management.link"
+  zone_id = 
+  name    = "hrapp-1"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.hr-app-1.public_ip]
 }
 
 resource "aws_route53_record" "hrapp-ns-2" {
-  zone_id = aws_route53_zone.hrapp-route53.zone_id
-  name    = "hrapp-2.kensko-hr-management.link"
+  zone_id = 
+  name    = "hrapp-2"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.hr-app-2.public_ip]
 }
 
 resource "aws_route53_record" "hrapp-ns-3" {
-  zone_id = aws_route53_zone.hrapp-route53.zone_id
-  name    = "hrapp-3.kensko-hr-management.link"
+  zone_id = 
+  name    = "hrapp-3"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.hr-app-3.public_ip]
 }
 
 resource "aws_route53_record" "hrapp-ns-4" {
-  zone_id = aws_route53_zone.hrapp-route53.zone_id
-  name    = "prometheus.kensko-hr-management.link"
+  zone_id =
+  name    = "prometheus"
   type    = "A"
   ttl     = "300"
   records = [aws_instance.hr-app-4.public_ip]
